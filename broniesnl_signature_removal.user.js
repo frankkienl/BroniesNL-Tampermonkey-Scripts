7f6c11bbeb7ec216a1f29c65ea6ff82f05a5a4e8
Frank Bouwens <frankkie12345@gmail.com>
2015-08-04 10:07:05 +0200

    Update broniesnl_signature_removal.user.js

--- broniesnl_signature_removal.user.js
+++ broniesnl_signature_removal.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BroniesNL - Remove Signatures
 // @namespace    http://frankkie.nl/
-// @version      0.2
+// @version      0.3
 // @description  Remove all signatures on the forum
 // @author       FrankkieNL
 // @match        http://bronies.nl/e107_plugins/forum/forum_viewtopic.php*
