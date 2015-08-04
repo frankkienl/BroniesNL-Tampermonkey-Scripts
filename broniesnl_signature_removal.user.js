384aabe824ad42e4b2c801b8d7dbe7a64803f18a
Frank Bouwens <frankkie12345@gmail.com>
2015-08-04 09:36:56 +0200

    Create broniesnl_signature_removal.user.js

---
+++ broniesnl_signature_removal.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         BroniesNL - Remove Signatures
+// @namespace    http://frankkie.nl/
+// @version      0.2
+// @description  Remove all signatures on the forum
+// @author       FrankkieNL
+// @match        http://bronies.nl/e107_plugins/forum/forum_viewtopic.php*
+// @grant        none
+// ==/UserScript==
+
+//Signatures and stuff below avatar
+var smalls = document.getElementsByClassName('smalltext');
+for (var i=0; i<smalls.length; i++){smalls[i].style.display='none';}
+
+//horizontal lines
+var hrs = document.getElementsByTagName('hr');
+for (var i=0; i<hrs.length;i++){hrs[i].style.display='none';}
