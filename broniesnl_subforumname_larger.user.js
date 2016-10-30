7413881c41c257b738eaeb05d28d8f8566772dcf
Frank Bouwens <frankkie12345@gmail.com>
2016-10-30 22:45:22 +0100

    Support HTTPS

--- broniesnl_subforumname_larger.user.js
+++ broniesnl_subforumname_larger.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Enlarge the name of the current subforum
 // @author       FrankkieNL
-// @match        http://bronies.nl/e107_plugins/forum/forum_viewforum.php?*
+// @match        http*://bronies.nl/e107_plugins/forum/forum_viewforum.php?*
 // @grant        none
 // @downloadURL  https://github.com/frankkienl/BroniesNL-Tampermonkey-Scripts/raw/master/broniesnl_subforumname_larger.user.js
 // @supportURL   https://github.com/frankkienl/BroniesNL-Tampermonkey-Scripts/
