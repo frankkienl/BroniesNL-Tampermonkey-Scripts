9a04f99b7ad746da4a695dfe6acc3586ab3b743f
Frank Bouwens <frankkie12345@gmail.com>
2016-10-30 22:46:16 +0100

    Support HTTPS

--- broniesnl_topic_larger.user.js
+++ broniesnl_topic_larger.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Make the topic of the thread larger.
 // @author       You
-// @match        http://bronies.nl/e107_plugins/forum/forum_viewtopic.php?*
+// @match        http*://bronies.nl/e107_plugins/forum/forum_viewtopic.php?*
 // @grant        none
 // @downloadURL  https://github.com/frankkienl/BroniesNL-Tampermonkey-Scripts/raw/master/broniesnl_topic_larger.user.js
 // @supportURL   https://github.com/frankkienl/BroniesNL-Tampermonkey-Scripts/
