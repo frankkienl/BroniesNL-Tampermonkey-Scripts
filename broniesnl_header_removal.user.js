a1a415210663d9f5bb2f96bdb0cfe43ff393fad1
Frank Bouwens <frankkie12345@gmail.com>
2016-10-30 22:44:57 +0100

    Support HTTPS

--- broniesnl_header_removal.user.js
+++ broniesnl_header_removal.user.js
@@ -4,7 +4,7 @@
 // @version      0.4
 // @description  Remove the huge header.
 // @author       FrankkieNL
-// @match        http://bronies.nl/*
+// @match        http*://bronies.nl/*
 // @grant        none
 // @downloadURL  https://github.com/frankkienl/BroniesNL-Tampermonkey-Scripts/raw/master/broniesnl_header_removal.user.js
 // @supportURL   https://github.com/frankkienl/BroniesNL-Tampermonkey-Scripts/
